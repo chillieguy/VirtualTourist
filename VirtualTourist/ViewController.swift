6b80600580a1ad9bc69068dc168c5073b368c239
Chuck Underwood <chillieguy@live.com>
2018-10-23 14:25:20 -0700

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Chuck Underwood on 10/23/18.
+//  Copyright © 2018 Chuck Underwood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
