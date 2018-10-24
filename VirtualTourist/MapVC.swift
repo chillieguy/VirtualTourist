5e34d1424810c9e5486c877335437afc478b1fe8
Chuck Underwood <chillieguy@live.com>
2018-10-24 06:24:27 -0700

    Add UILongPressGestureRecognizer to drop pin at touch point

--- VirtualTourist/MapVC.swift
+++ VirtualTourist/MapVC.swift
@@ -11,14 +11,35 @@ import MapKit
 
 class MapVC: UIViewController {
     
+    @IBOutlet weak var map: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        map.delegate = self
         
+        setupGestureRecognizer()
+    }
+    
+    func setupGestureRecognizer() {
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
+        map.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    @objc func handleTap(_ gestureRecognizer: UILongPressGestureRecognizer) {
+        let location = gestureRecognizer.location(in: map)
+        let coordinate = map.convert(location, toCoordinateFrom: map)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        map.addAnnotation(annotation)
     }
     
     
 
+}
+
+extension MapVC: MKMapViewDelegate {
+    
 }
 
