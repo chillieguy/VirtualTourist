d18b4199c45bd164c53b0318ff8466c754cce66d
Chuck Underwood <chillieguy@live.com>
2018-12-22 18:29:23 -0800

    Update PhotoCollectionViewCell.swift

--- VirtualTourist/ViewControllers/PhotoCollectionViewCell.swift
+++ VirtualTourist/ViewControllers/PhotoCollectionViewCell.swift
@@ -11,7 +11,5 @@ import UIKit
 class PhotoCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var photoCollectionViewCellImage: UIImageView!
-    @IBOutlet weak var photoCollectionViewCellActivityIndicator: UIActivityIndicatorView!
-    
-    
+
 }
