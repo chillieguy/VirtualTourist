2080ed5d4ee5192c7c657281ada1545fadae0a0a
Chuck Underwood <chillieguy@live.com>
2018-12-22 16:52:06 -0800

    Remove unused functions

--- VirtualTourist/Model/DataController.swift
+++ VirtualTourist/Model/DataController.swift
@@ -9,28 +9,114 @@
 import Foundation
 import CoreData
 
-class DataController {
-    let persistentContainer: NSPersistentContainer
+struct DataController {
     
-    var viewContext: NSManagedObjectContext {
-        return persistentContainer.viewContext
-    }
+    private let model: NSManagedObjectModel
+    internal let coordinator: NSPersistentStoreCoordinator
+    private let modelURL: URL
+    internal let dbURL: URL
+    internal let persistingContext: NSManagedObjectContext
+    internal let backgroundContext: NSManagedObjectContext
+    let context: NSManagedObjectContext
     
-    init(modelName: String) {
-        persistentContainer = NSPersistentContainer(name: modelName)
-    }
-    
-    func load(completion: (() -> Void)? = nil) {
-        persistentContainer.loadPersistentStores { (storeDescription, error) in
-            guard error == nil else {
-                fatalError(error!.localizedDescription)
-            }
-            completion?()
+    init?(modelName: String) {
+        
+        guard let modelURL = Bundle.main.url(forResource: modelName, withExtension: "momd") else {
+            print("Unable to find \(modelName)in the main bundle")
+            return nil
         }
+        self.modelURL = modelURL
+        
+        guard let model = NSManagedObjectModel(contentsOf: modelURL) else {
+            print("unable to create a model from \(modelURL)")
+            return nil
+        }
+        self.model = model
+        
+        coordinator = NSPersistentStoreCoordinator(managedObjectModel: model)
+    
+        persistingContext = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+        persistingContext.persistentStoreCoordinator = coordinator
+        
+        context = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        context.parent = persistingContext
+        
+        backgroundContext = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+        backgroundContext.parent = context
+        
+        let fileManager = FileManager.default
+        
+        guard let docUrl = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            print("Unable to reach the documents folder")
+            return nil
+        }
+        
+        self.dbURL = docUrl.appendingPathComponent("model.sqlite")
+        
+        let options = [NSInferMappingModelAutomaticallyOption: true,NSMigratePersistentStoresAutomaticallyOption: true]
+        
+        do {
+            try addStoreCoordinator(NSSQLiteStoreType, configuration: nil, storeURL: dbURL, options: options as [NSObject : AnyObject]?)
+        } catch {
+            print("unable to add store at \(dbURL)")
+        }
+    }
+    
+    func addStoreCoordinator(_ storeType: String, configuration: String?, storeURL: URL, options : [NSObject:AnyObject]?) throws {
+        try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: dbURL, options: nil)
     }
     
 }
 
+internal extension DataController  {
+    
+    func dropAllData() throws {
+
+        try coordinator.destroyPersistentStore(at: dbURL, ofType: NSSQLiteStoreType , options: nil)
+        try addStoreCoordinator(NSSQLiteStoreType, configuration: nil, storeURL: dbURL, options: nil)
+    }
+}
+
 extension DataController {
     
+    typealias BackgroundBatch = (_ workerContext: NSManagedObjectContext) -> ()
+    
+    func performBackgroundBatchOperation(_ batch: @escaping BackgroundBatch) {
+        
+        backgroundContext.perform() {
+            
+            batch(self.backgroundContext)
+            do {
+                try self.backgroundContext.save()
+            } catch {
+                fatalError("Error while saving backgroundContext: \(error)")
+            }
+        }
+    }
 }
+
+extension DataController {
+    
+    func save() {
+    
+        context.performAndWait() {
+            
+            if self.context.hasChanges {
+                do {
+                    try self.context.save()
+                } catch {
+                    fatalError("Error while saving main context: \(error)")
+                }
+                
+                self.persistingContext.perform() {
+                    do {
+                        try self.persistingContext.save()
+                    } catch {
+                        fatalError("Error while saving persisting context: \(error)")
+                    }
+                }
+            }
+        }
+    }
+}
+
