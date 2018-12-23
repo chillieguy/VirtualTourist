5541ae397576c3031ef4df02beda15659092e6fe
Chuck Underwood <chillieguy@live.com>
2018-12-22 18:27:10 -0800

    Update Pin+Extensions.swift

--- VirtualTourist/Model/Pin+Extensions.swift
+++ VirtualTourist/Model/Pin+Extensions.swift
@@ -10,11 +10,28 @@ import Foundation
 import CoreData
 
 extension Pin {
-    public override func awakeFromInsert() {
-        super.awakeFromInsert()
-        createdDate = Date()
-        id = UUID()
-        print(id!)
-        
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Pin> {
+        return NSFetchRequest<Pin>(entityName: "Pin")
     }
+    
+    @NSManaged public var latitude: Double
+    @NSManaged public var longitude: Double
+    @NSManaged public var photos: NSSet?
+    
+}
+
+extension Pin {
+    
+    @objc(addPhotosObject:)
+    @NSManaged public func addToPhotos(_ value: Photo)
+    
+    @objc(removePhotosObject:)
+    @NSManaged public func removeFromPhotos(_ value: Photo)
+    
+    @objc(addPhotos:)
+    @NSManaged public func addToPhotos(_ values: NSSet)
+    
+    @objc(removePhotos:)
+    @NSManaged public func removeFromPhotos(_ values: NSSet)
+
 }
